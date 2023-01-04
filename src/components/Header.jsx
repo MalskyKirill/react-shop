4acbc1d4ddef2f08bd241ec32d14bdeafc4f9bbc
Kirill Malskiy <sabbat@mail.ru>
2023-01-04 13:14:50 +0300

    подготовка проекта

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header () {
+  return (
+    <nav className="#43a047 green darken-1">
+      <div className="nav-wrapper">
+        <a href="#" className="brand-logo">React shop</a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><a href="#">Repo</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export {Header};
