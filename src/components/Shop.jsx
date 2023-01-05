53dff576d7b817e72f741a57c3644586e5ee0830
Kirill Malskiy <sabbat@mail.ru>
2023-01-05 22:44:50 +0300

    создание корзины

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -4,11 +4,13 @@ import { API_KEY, API_URL } from '../config';
 import { Preloader } from './Preloader';
 import { GoodsList } from './GoodsList';
 import { Cart } from './Cart';
+import {BasketList} from './BasketList';
 
 function Shop() {
   const [goods, setGoods] = useState([]);
   const [loading, setLoading] = useState(true);
   const [order, setOrder] = useState([]);
+  const [isBasketShow, setBasketShow] = useState(false);
 
   const addToBasket = (item) => {
     const itemIndex = order.findIndex((orderItem) => orderItem.id === item.id);
@@ -35,6 +37,10 @@ function Shop() {
     }
   };
 
+  const handleBasketShow = () => {
+    setBasketShow(!isBasketShow);
+  }
+
   useEffect(function getGoods() {
     fetch(API_URL, {
       headers: {
@@ -50,12 +56,15 @@ function Shop() {
   console.log(order);
   return (
     <main className='container content'>
-      <Cart quantity={order.length} />
+      <Cart quantity={order.length} handleBasketShow={handleBasketShow}/>
       {loading ? (
         <Preloader />
       ) : (
         <GoodsList goods={goods} addToBasket={addToBasket} />
       )}
+      {
+        isBasketShow && <BasketList order={order} handleBasketShow={handleBasketShow}/>
+      }
     </main>
   );
 }
