80c45fdfbd6ae1bbae46d657d0e23a2eaff29d39
Kirill Malskiy <sabbat@mail.ru>
2023-01-06 13:42:51 +0300

    добавил ссылки на repo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,21 @@
-function Footer () {
-
+function Footer() {
   return (
-    <footer className="page-footer green lighten-3">
-      <div className="footer-copyright">
-        <div className="container container-footer">
-        © {new Date().getFullYear()} Copyright Text
-        <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+    <footer className='page-footer green lighten-3'>
+      <div className='footer-copyright'>
+        <div className='container container-footer'>
+          © {new Date().getFullYear()} Copyright Text
+          <a
+            className='grey-text text-lighten-4 right'
+            href='https://malskykirill.github.io/react-shop/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            Repo
+          </a>
         </div>
       </div>
     </footer>
   );
 }
 
-export {Footer};
+export { Footer };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,24 @@
-function Header () {
+function Header() {
   return (
-    <nav className="#43a047 green darken-1">
-      <div className="nav-wrapper">
-        <a href="#" className="brand-logo">React shop</a>
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><a href="#">Repo</a></li>
+    <nav className='#43a047 green darken-1'>
+      <div className='nav-wrapper'>
+        <a href='/' className='brand-logo'>
+          React shop
+        </a>
+        <ul id='nav-mobile' className='right hide-on-med-and-down'>
+          <li>
+            <a
+              href='https://malskykirill.github.io/react-shop/'
+              target='_blank'
+              rel="noreferrer"
+            >
+              Repo
+            </a>
+          </li>
         </ul>
       </div>
     </nav>
   );
 }
 
-export {Header};
+export { Header };
