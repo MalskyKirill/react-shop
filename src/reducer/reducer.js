3a46253eb4413f84b455d34630dbd9950aa15121
Kirill Malskiy <sabbat@mail.ru>
2023-01-06 18:26:48 +0300

    создал контекст с провайдером и редьюсер

---
+++ src/reducer/reducer.js
@@ -0,0 +1,86 @@
+export function reducer(state, { type, payload }) {
+  switch (type) {
+    case 'ADD_TO_BASKET': {
+      const itemIndex = state.order.findIndex(
+        (orderItem) => orderItem.id === payload.id
+      );
+
+      let newOrder = null;
+      if (itemIndex < 0) {
+        const newItem = {
+          ...payload,
+          quantity: 1,
+        };
+        newOrder = [...state.order, newItem];
+      } else {
+        newOrder = state.order.map((orderItem, index) => {
+          if (index === itemIndex) {
+            return {
+              ...orderItem,
+              quantity: orderItem.quantity + 1,
+            };
+          } else {
+            return orderItem;
+          }
+        });
+      }
+
+      return {
+        ...state,
+        order: newOrder,
+        alertName: payload.name,
+      };
+    }
+    case 'INQ_QUANTITY': {
+      const newOrder = state.order.map((el) => {
+        if (el.id === payload.id) {
+          const newQuantity = el.quantity + 1;
+          return {
+            ...el,
+            quantity: newQuantity,
+          };
+        } else {
+          return el;
+        }
+      });
+      return {
+        ...state,
+        order: newOrder
+      }
+    }
+    case 'DEC_QUANTITY': {
+      const newOrder = state.order.map((el) => {
+        if (el.id === payload.id) {
+          const newQuantity = el.quantity + 1;
+          return {
+            ...el,
+            quantity: newQuantity,
+          };
+        } else {
+          return el;
+        }
+      });
+      return {
+        ...state,
+        order: newOrder
+      }
+    }
+    case 'TOGGLE_BASKET_SHOW':
+      return {
+        ...state,
+        isBasketShow: !state.isBasketShow,
+      }
+    case 'CLOSE_ALERT':
+      return {
+        ...state,
+        alertName: '',
+      };
+    case 'REMOVE_FROM_BASKET':
+      return {
+        ...state,
+        order: state.order.filter((el) => el.id !== payload),
+      };
+    default:
+      return state;
+  }
+}
